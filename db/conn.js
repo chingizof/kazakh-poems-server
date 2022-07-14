0efacb73a9c54bc53358a857e742133a28567e2d
Mister-718 <59221853+Mister-718@users.noreply.github.com>
2022-07-14 11:34:47 +0600

    initial commit

---
+++ db/conn.js
@@ -0,0 +1,28 @@
+import { MongoClient as mongo } from "mongodb"
+// import 'dotenv/config';
+
+// const url = process.env['MONGODB_URI']
+const url = "mongodb+srv://cheenv:Net987654321$@salem.wrbh3.mongodb.net/?retryWrites=true&w=majority"
+let db;
+
+const connect = () => {
+    mongo.connect(
+        url,
+        {
+            usenewUrlParser: true,
+            useUnifiedTopology: true,
+        },
+        (err, client) => {
+            if (err) {
+                console.error(err)
+                return
+            }
+            db = client.db("nfactorial")
+            console.log('connected')
+        }
+    )
+}
+
+const getDB = () => db
+
+export { connect, getDB }
